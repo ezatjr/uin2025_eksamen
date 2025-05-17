3399326a18c797bd3348688ebbbbfa31cbec444e
ezatjr <Ezatj@hiof.no>
2025-05-17 23:33:26 +0500

    Opprettet WishlistContext for å legge til i ønskelister

---
+++ src/context/WishlistContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useContext, ReactNode } from 'react';
+import { WishlistItem } from '../types';
+
+interface WishlistContextType {
+  wishlist: WishlistItem[];
+  addToWishlist: (item: WishlistItem) => void;
+  removeFromWishlist: (id: string) => void;
+  isInWishlist: (id: string) => boolean;
+}
+
+const WishlistContext = createContext<WishlistContextType | undefined>(undefined);
+
+export function WishlistProvider({ children }: { children: ReactNode }) {
+  const [wishlist, setWishlist] = useState<WishlistItem[]>([]);
+
+  const addToWishlist = (item: WishlistItem) => {
+    if (!isInWishlist(item.id)) {
+      setWishlist((prevItems) => [...prevItems, item]);
+    }
+  };
+
+  const removeFromWishlist = (id: string) => {
+    setWishlist((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const isInWishlist = (id: string) => {
+    return wishlist.some((item) => item.id === id);
+  };
+
+  return (
+    <WishlistContext.Provider
+      value={{ wishlist, addToWishlist, removeFromWishlist, isInWishlist }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+}
+
+export function useWishlist() {
+  const context = useContext(WishlistContext);
+  if (context === undefined) {
+    throw new Error('useWishlist must be used within a WishlistProvider');
+  }
+  return context;
+}
